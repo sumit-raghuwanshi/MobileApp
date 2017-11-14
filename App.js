2e7fd5a7ccd7a144e3b09854d5baddca36600815
MarcoSamson21 <marco.samson21@gmail.com>
2017-11-14 17:54:32 -0500

    Add login screen

--- App.js
+++ App.js
@@ -1,37 +1,50 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  StatusBar
 } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 export default class App extends Component<{}> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <StatusBar barStyle="light-content"/>
+
+        <Image style={styles.background} source={require("./img/login/login-bg.png")} />
+        <View style={styles.formContainer}>
+
+          <View style={styles.imageContainer}>
+            <Image source={require("./img/logo.png")}/>
+          </View>
+
+          <TextInput
+            placeholder="User Name"
+            placeholderTextColor="#4D4D4D"
+            style={styles.inputField} />
+
+          <View style={{ height: 16 }} />
+
+          <TextInput
+            placeholder="Password"
+            placeholderTextColor="#4D4D4D"
+            style={styles.inputField} />
+
+          <View style={{ height: 15 }} />
+
+          <View style={styles.buttonContainer}>
+            <View style={{ flex: 1 }}/>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Login</Text>
+            </TouchableOpacity>
+          </View>
+
+        </View>
       </View>
     );
   }
@@ -40,9 +53,40 @@ export default class App extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'center'
+  },
+  background: {
+    position: "absolute"
+  },
+  formContainer: {
+    height: 345.6,
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    paddingHorizontal: 22.05,
+    justifyContent: 'center'
+  },
+  imageContainer: {
+    alignItems: 'center'
+  },
+  inputField: {
+    height: 30,
+    backgroundColor: '#FFFFFF',
+    color: '#4D4D4D',
+    fontSize: 14,
+    paddingHorizontal: 10
+  },
+  buttonContainer: {
+    flexDirection: 'row'
+  },
+  button: {
+    height: 30,
+    backgroundColor: '#E88A18',
+    paddingHorizontal: 28,
+    justifyContent: 'center'
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 14,
+    fontWeight: '600'
   },
   welcome: {
     fontSize: 20,
